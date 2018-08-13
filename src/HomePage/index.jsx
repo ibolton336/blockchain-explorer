615e59b4b364f9c4033722f2614366402d43647c
Ian Bolton <ibolton336@gmail.com>
2018-08-13 18:54:12 -0400

    add dropdown change event handler

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -2,27 +2,47 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { userdataActions } from "../_actions/userdata.actions";
 import { SearchResults } from "../_components/SearchResults";
-import {Dropdown} from "semantic-ui-react";
-  const options = [
-    {
-      text: 'Search Transaction',
-      value: 'transaction'
-    }
-  ]
-    
+import { Dropdown } from "semantic-ui-react";
+
+const options = [
+  {
+    text: 'Search block',
+    value: 'block'
+  },
+  {
+    text: 'Search transaction',
+    value: 'transaction'
+  },
+  {
+    text: 'View latest',
+    value: 'latest'
+  }
+]
+
 class ConnectedHomePage extends React.Component {
-  componentDidMount() {}
+  constructor(props) {
+    super(props);
+    this.state = { displayOption: '' };
+  }
+  componentDidMount() { }
+
+  setDisplayOption = (e, data) => {
+    this.setState({ displayOption: data.value })
+  }
   render() {
     const { updateUserdata, userdata } = this.props;
     return (
       <div>
-      <Dropdown placeholder='Select Friend' fluid selection options={options} />
+        <Dropdown placeholder='Select option'
+          fluid selection options={options}
+          onChange={this.setDisplayOption}
+        />
 
-      <SearchResults
-        className="form-container"
+        <SearchResults
+          className="form-container"
         // onUpdateData={updateUserdata}
         // userdata={userdata.userdata}
-      />
+        />
 
       </div>
     );
