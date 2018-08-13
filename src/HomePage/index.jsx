e9368b6fb7ff7e5a6aa68b93005a83cad83330e1
Ian Bolton <ibolton336@gmail.com>
2018-08-13 18:47:11 -0400

    start implementation of drop down selector

--- src/HomePage/index.jsx
+++ src/HomePage/index.jsx
@@ -2,18 +2,29 @@ import * as React from "react";
 import { connect } from "react-redux";
 import { userdataActions } from "../_actions/userdata.actions";
 import { SearchResults } from "../_components/SearchResults";
-
+import {Dropdown} from "semantic-ui-react";
+  const options = [
+    {
+      text: 'Search Transaction',
+      value: 'transaction'
+    }
+  ]
+    
 class ConnectedHomePage extends React.Component {
   componentDidMount() {}
-
   render() {
     const { updateUserdata, userdata } = this.props;
     return (
+      <div>
+      <Dropdown placeholder='Select Friend' fluid selection options={options} />
+
       <SearchResults
         className="form-container"
         // onUpdateData={updateUserdata}
         // userdata={userdata.userdata}
       />
+
+      </div>
     );
   }
 }
